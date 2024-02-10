4f344118c9cec81476bff2cee5122857fe3e0ebc
JuanCM <juancampuzano2356@gmail.com>
2024-02-10 11:50:43 -0500

    PHP con HTML

---
+++ html/php-html.php
@@ -0,0 +1,28 @@
+<?php
+
+$zorritos = ["https://randomfox.ca/images/1.jpg", "https://randomfox.ca/images/2.jpg", "https://randomfox.ca/images/3.jpg"];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h1>Hola esto es HTML desde PHP</h1>
+    <?php
+
+    foreach ($zorritos as $zorrito) {
+        echo "<img src='$zorrito'>";
+    }
+
+    ?>
+
+</body>
+
+</html>
